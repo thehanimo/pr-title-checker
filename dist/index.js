2f8d7d99310c67199efc37173addbc0e09ccb1c7
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-13 14:14:34 +0200

    concat url

--- dist/index.js
+++ dist/index.js
@@ -746,7 +746,7 @@ async function run() {
 
     const buildCommentBody = () => {
       const emptySpace = 'https://support.apps.darva.com/browse/SINAPPSHAB-'
-      return emptySpace .concat([emptySpace, JIRA_TICKETS]).join('\n')
+      return emptySpace.concat([JIRA_TICKETS]).join('\n')
     }
 
     _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(` PR Title ${title}`)
