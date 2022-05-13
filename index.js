11ff562916611096d18c995f9d25c70ed18e53b4
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-13 12:02:54 +0200

    essai1

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-const issue_number = github.context.issue.number;
-const { Octokit } = require("@octokit/action");
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-let octokit;
-let  JIRA_TICKETS = []
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+const issue_number = github.context.issue.number
+const { Octokit } = require('@octokit/action')
+
+let octokit
+let JIRA_TICKETS = []
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const title = github.context.payload.pull_request.title;
-    const labels = github.context.payload.pull_request.labels;
-    const getJiraTicketsFromPrTitle = ( ) => {
+    const title = github.context.payload.pull_request.title
+    const labels = github.context.payload.pull_request.labels
+    const getJiraTicketsFromPrTitle = () => {
       //const trimmedTitle=title.replaceAll(" ","")
-      JIRA_TICKETS  = title.split('-')[0].split('|')
-      core.info( ` JIRA Ticket ${JIRA_TICKETS}`)
+      JIRA_TICKETS = title.split('-')[0].split('|')
+      core.info(` JIRA Ticket ${JIRA_TICKETS}`)
     }
-    core.info( ` PR Title ${title}`)
-    let pattern = /\d{4,5}/;
-    const titleContainsJiraNumbers = pattern.test(title, "i");
-
+    core.info(` PR Title ${title}`)
+    let pattern = /\d{4,5}/
+    const titleContainsJiraNumbers = pattern.test(title, 'i')
+    let body
     if (titleContainsJiraNumbers) {
       getJiraTicketsFromPrTitle()
       core.setOutput('JIRA_TICKETS', JIRA_TICKETS)
+      body = buildCommentBody()
+      createOrUpdateComment(body)
     } else {
       await addLabel('NotLinkedToJira')
       core.setOutput('JIRA_TICKETS', [])
     }
-
   } catch (error) {
-    core.info(error);
+    core.info(error)
   }
 }
+async function buildCommentBody() {
+  const emptySpace = 'hello'
+  return emptySpace
+}
+async function createOrUpdateComment(body: string): Promise<void> {
+  const { owner, name: repo, prNumber: issue_number, octokit } = this
 
+  await octokit.rest.issues.createComment({
+    owner,
+    repo,
+    issue_number,
+    body,
+  })
+}
 async function addLabel(name) {
-  core.info(`Adding label (${name}) to PR...`);
+  core.info(`Adding label (${name}) to PR...`)
   let addLabelResponse = await octokit.issues.addLabels({
     owner,
     repo,
     issue_number,
     labels: [name],
-  });
-  core.info(`Added label (${name}) to PR - ${addLabelResponse.status}`);
+  })
+  core.info(`Added label (${name}) to PR - ${addLabelResponse.status}`)
 }
 
-
 try {
-  octokit = new Octokit();
+  octokit = new Octokit()
 } catch (e) {
-  handleOctokitError(e);
+  handleOctokitError(e)
 }
 
 if (octokit) {
-  run();
+  run()
 }
