4057568c3aa4eb2eb4ac07a01e3c5a128a7e7524
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-19 09:53:27 +0200

    test

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
       await addLabel('NotLinkedToJira')
       const splittedBody = firstbody.split(separator)
       const userdPrBody = splittedBody.length === 1 ? splittedBody[0] : splittedBody[1]
-      core.info(userdPrBody)
+      core.info(`userdPrBody ${userdPrBody}`)
       await createOrUpdateComment(userdPrBody)
       core.setOutput('JIRA_TICKETS', [])
     }
