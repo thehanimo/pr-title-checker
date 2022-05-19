d76cc4cd75a27aee2f0aaeb2906d1f846be79ffa
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-19 11:06:27 +0200

    font size 3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function run() {
       let tab=[]
       let urlWithSeparator=''
       JIRA_TICKETS.map((e)=> {
-      tab.push('\r\n',urlTicket.concat(e).fontsize(7))
+      tab.push('\r\n',(urlTicket.concat(e)).fontsize(3))
       })
 
       if (firstbody==undefined){
